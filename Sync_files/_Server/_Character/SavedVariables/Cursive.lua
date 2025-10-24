9916d07b8818c49202cc269fe5939905a99d59fe
Ungweliant <>
2025-10-24 17:01:20 +0200

    added some addons

---
+++ Sync_files/_Server/_Character/SavedVariables/Cursive.lua
@@ -0,0 +1,27 @@
+
+CursiveDB = {
+	["profiles"] = {
+		["Default"] = {
+			["curseordering"] = "Expiring latest -> soonest",
+			["anchor"] = "RIGHT",
+			["clickthrough"] = true,
+			["filterhascurse"] = true,
+			["detachedTooltip"] = {
+				["fontSizePercent"] = 1,
+			},
+			["y"] = 79.17000541148227,
+			["x"] = -216.9899928268625,
+			["healthwidth"] = 120,
+			["maxcurses"] = 6,
+			["shareddebuffs"] = {
+				["faeriefire"] = true,
+			},
+			["spacing"] = 5,
+			["showtitle"] = false,
+			["invertbars"] = true,
+			["scale"] = 1.2,
+			["curseshowdecimals"] = true,
+			["filterraidmark"] = true,
+		},
+	},
+}
